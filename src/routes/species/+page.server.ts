2d8a53132bbb282850786480d870d11a4a898bfa
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-12-11 10:43:55 -0500

    DB port from .env

--- src/routes/species/+page.server.ts
+++ src/routes/species/+page.server.ts
@@ -1,8 +1,10 @@
 import type { PageServerLoad } from './$types';
+import { DB_HOST, DB_PORT } from '$env/static/private';
 
 export const load: PageServerLoad = async ({ fetch }) => {
+	console.log(DB_PORT);
 	const fetchSpecieslist = async () => {
-		const data = await fetch('http://localhost:55835/api/data/species');
+		const data = await fetch(`http://${DB_HOST}:${DB_PORT}/api/data/species`);
 		const linelists = await data.json();
 		return linelists;
 	};
--- src/routes/species/[id]/+page.server.ts
+++ src/routes/species/[id]/+page.server.ts
@@ -1,15 +1,17 @@
 import type { PageServerLoad } from './$types';
+import { DB_HOST, DB_PORT } from '$env/static/private';
 
 export const load: PageServerLoad = async ({ fetch, params }) => {
+	const URL = `http://${DB_HOST}:${DB_PORT}/api/data`;
 	// console.log(params);
 	const fetchSpecies = async (id: string) => {
-		const data = await fetch(`http://localhost:55835/api/data/species/${id}`);
+		const data = await fetch(`${URL}/species/${id}`);
 		const linelists = await data.json();
 		return linelists;
 	};
 
 	const fetchSpeciesMeta = async (id: string) => {
-		const data = await fetch(`http://localhost:55835/api/data/species-metadata/${id}`);
+		const data = await fetch(`${URL}/species-metadata/${id}`);
 		const linelists = await data.json();
 		return linelists;
 	};
