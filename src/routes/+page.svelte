5cc7438a0a611ce46506e632e11f196484fb6506
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-12-11 17:13:18 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
-<div class="hero min-h-screen bg-base-200">
+<div class="hero bg-base-200 card shadow-xl h-full">
 	<div class="hero-content text-center">
 		<div class="max-w-md">
 			<h1 class="text-5xl font-bold">SAAGAdb</h1>
 			<p class="py-6">Species-Agnostic Automated Gas Analyzer</p>
-			<button class="btn btn-primary">Get Started</button>
+			<a href="/species" class="btn btn-primary">Get Started</a>
 		</div>
 	</div>
 </div>
