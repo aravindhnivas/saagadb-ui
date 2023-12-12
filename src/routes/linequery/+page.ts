09315c0519c6dd603633098206a2ed2fe0d27e94
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-12-12 16:18:28 -0500

    bug fix

--- src/routes/linequery/+page.ts
+++ src/routes/linequery/+page.ts
@@ -3,7 +3,7 @@ export const load: PageLoad = async ({ url, fetch }) => {
 	const min = Number(url.searchParams.get('min_freq') ?? '0');
 	const max = Number(url.searchParams.get('max_freq') ?? '1');
 	const getLinequery = async () => {
-		const query = encodeURIComponent(`min_freq${min}&max_freq=${max}`);
+		const query = encodeURIComponent(`min_freq=${min}&max_freq=${max}`);
 		const res = await fetch(`/api/line/query?query=${query}`);
 		const data = await res.json();
 		return data;
