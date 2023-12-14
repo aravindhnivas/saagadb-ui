1c4cf7a63e914ce786f0e4d7d48cc4a4afa0ff1d
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-12-14 17:27:54 -0500

    Update +page.ts

--- src/routes/linequery/+page.ts
+++ src/routes/linequery/+page.ts
@@ -2,8 +2,8 @@ import type { PageLoad } from './$types';
 import { error } from '@sveltejs/kit';
 export const load: PageLoad = async ({ url, fetch }) => {
 	const getLinequery = async () => {
-		const min_freq = url.searchParams.get('min_freq');
-		const max_freq = url.searchParams.get('max_freq');
+		const min_freq = url.searchParams.get('min_freq') ?? '0';
+		const max_freq = url.searchParams.get('max_freq') ?? '0';
 		if (!min_freq || !max_freq) throw error(500, 'min_freq and max_freq must be specified');
 		const queryString = new URLSearchParams({ min_freq, max_freq }).toString();
 		const res = await fetch(`/api/line/query?query=${encodeURIComponent(queryString)}`);
