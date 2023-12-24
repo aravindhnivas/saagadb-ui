7cfec7eb3b5406735117e737eaaac0be88fd7cb3
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-12-23 20:03:59 -0500

    Update +server.ts

--- src/routes/logout/+server.ts
+++ src/routes/logout/+server.ts
@@ -3,13 +3,6 @@ import { logged_in } from '$lib/utils';
 import type { RequestHandler } from './$types';
 export const POST: RequestHandler = async ({ cookies, locals }) => {
 	logged_in.set('');
-	cookies.set('token', '', {
-		maxAge: 0,
-		path: '/',
-		domain: locals.domain,
-		httpOnly: true,
-		sameSite: 'lax'
-		// secure: false
-	});
+	cookies.delete('token', { path: '/', domain: locals.domain });
 	redirect(303, '/');
 };
