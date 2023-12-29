bb2b26093104b415d6639e17a39a81fd439b3bb8
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-12-29 14:32:37 -0500

    format

--- saagadb-ui.config.cjs
+++ saagadb-ui.config.cjs
@@ -2,16 +2,18 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    apps: [{
-        name: 'saagadb-ui',
-        script: 'build/index.js',
-        interpreter: 'node',
-        interpreter_args: '-r dotenv/config',
-        env: {
-            PORT: process.env.PORT,
-            ORIGIN: "http://herzberg.mit.edu",
-            PROTOCOL_HEADER: "x-forwarded-proto",
-            HOST_HEADER: "x-forwarded-host"
-        },
-    }],
+	apps: [
+		{
+			name: 'saagadb-ui',
+			script: 'build/index.js',
+			interpreter: 'node',
+			interpreter_args: '-r dotenv/config',
+			env: {
+				PORT: process.env.PORT,
+				ORIGIN: 'http://herzberg.mit.edu',
+				PROTOCOL_HEADER: 'x-forwarded-proto',
+				HOST_HEADER: 'x-forwarded-host'
+			}
+		}
+	]
 };
--- src/routes/logout/+server.ts
+++ src/routes/logout/+server.ts
@@ -4,11 +4,11 @@ import type { RequestHandler } from './$types';
 export const POST: RequestHandler = async ({ locals, cookies }) => {
 	logged_in.set('');
 	cookies.delete('token', {
-		path: '/', 
-		domain: locals.domain, 
+		path: '/',
+		domain: locals.domain,
 		httpOnly: true,
-		sameSite: "lax",
-		secure: false 
+		sameSite: 'lax',
+		secure: false
 	});
 	redirect(303, '/');
 };
