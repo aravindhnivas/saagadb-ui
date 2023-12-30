7119f4ad998dfc07e08d82c50cdf534665dcdeb2
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-12-30 17:44:04 -0500

    Update navitems.svelte

--- src/lib/layout/nav/navitems.svelte
+++ src/lib/layout/nav/navitems.svelte
@@ -1,12 +1,13 @@
 <li><a href="/">Home</a></li>
 <li><a href="/species">Species</a></li>
 <li><a href="/linequery">Line-query</a></li>
-<!-- <li tabindex="0">
+<!-- svelte-ignore a11y-positive-tabindex -->
+<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+<li tabindex="0" style="z-index: 1;">
 	<details>
-		<summary>Database</summary>
-		<ul class="p-2">
-			<li><a>Submenu 1</a></li>
-			<li><a>Submenu 2</a></li>
+		<summary>Misc.</summary>
+		<ul class="p-2 w-[10rem]">
+			<li><a href="http://herzberg.mit.edu:8080" target="_blank">PDF-to-Table</a></li>
 		</ul>
 	</details>
-</li> -->
+</li>
