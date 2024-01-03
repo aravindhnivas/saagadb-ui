af3034a0ff734768f8f56e0161824fe50fc92f44
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-01-03 15:27:16 -0500

    Update +server.ts

--- src/routes/api/[name]/+server.ts
+++ src/routes/api/[name]/+server.ts
@@ -1,4 +1,4 @@
-import { error } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import { DB_URL } from '$lib/server';
 
@@ -10,5 +10,21 @@ export const GET: RequestHandler = async ({ params, url }) => {
 
 	const data = await res.json();
 	if (data.error) error(500, data.error);
-	return new Response(JSON.stringify(data), { status: 200 });
+
+	return json(data, { status: 200 });
+};
+
+export const POST: RequestHandler = async ({ params, request }) => {
+	const body = await request.json();
+	// const auth = request.headers.get('Authorization');
+	// console.log({ auth });
+	const res = await fetch(`${DB_URL}/data/${params.name}`, {
+		method: 'POST',
+		body: JSON.stringify(body)
+	});
+
+	const data = await res.json();
+	// console.log({ data });
+	if (data.error) error(500, data.error);
+	return json(data, { status: 201 });
 };
