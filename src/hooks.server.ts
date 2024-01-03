379c07425006d477d6a60a5135793e2c14d57f00
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-01-03 15:26:41 -0500

    Update hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -7,7 +7,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 	if (event.url.pathname.startsWith('/admin')) {
 		if (!event.locals.token) {
 			const fromUrl = event.url.pathname + event.url.search;
-			const message = 'You must be logged in to access this page.';
+			const message = 'You must be logged in to access the privileged admin page.';
 			redirect(303, `/login?redirectTo=${fromUrl}&message=${message}`);
 		}
 	}
