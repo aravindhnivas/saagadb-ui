f7e8cae1e593f97ec314c0b8e1d202f9faefb96d
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-01-06 20:32:52 -0500

    edit_mode localstorage store

--- src/lib/utils/stores.ts
+++ src/lib/utils/stores.ts
@@ -1,4 +1,5 @@
+import { persist, createLocalStorage } from '@macfja/svelte-persistent-store';
 import { writable } from 'svelte/store';
 
 export const logged_in = writable('');
-export const edit_mode = writable(false);
+export const edit_mode = persist(writable(false), createLocalStorage(), 'edit_mode');
