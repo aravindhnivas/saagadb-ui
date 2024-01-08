7181ebabb459212693f58d1e091c24fdbc838287
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-01-08 13:51:11 -0500

    Update +layout.server.ts

--- src/routes/admin/+layout.server.ts
+++ src/routes/admin/+layout.server.ts
@@ -1,30 +1,17 @@
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ fetch }) => {
+	const fetchFunc = async (url: string) => {
+		const res = await fetch(url);
+		if (!res.ok) return [];
+		return res.json();
+	};
+
 	const [species, linelist] = (await Promise.all([
-		fetch(`/api/species`)
-			.then((res) => res.json())
-			.catch((err) => {
-				console.log('error fetching species', err);
-				return [];
-			}),
-		fetch(`/api/linelist`)
-			.then((res) => res.json())
-			.catch((err) => {
-				console.log('error fetching linelist', err);
-				return [];
-			})
+		fetchFunc(`/api/species`),
+		fetchFunc(`/api/linelist`)
 	])) as [string[], { id: string; linelist_name: string }[]];
 
 	console.log({ species, linelist });
 	return { species, linelist };
-
-	// const linelist_res = await fetch(`/api/linelist`);
-	// if (!linelist_res.ok) return { linelist: [] };
-
-	// const linelist = (await linelist_res.json()) as {
-	// 	id: string;
-	// 	linelist_name: string;
-	// }[];
-	// return { linelist };
 };
