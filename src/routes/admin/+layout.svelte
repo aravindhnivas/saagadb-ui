7bd04ac22e661eccd7530020fba0f08337f6ed11
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-01-08 16:25:29 -0500

    Update +layout.svelte

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	const admin_menu = [
-		{ name: 'Dashboard', href: '/admin/dashboard' },
-		{ name: 'Create New user', href: '/admin/create_user' },
-		{ name: 'Database name', href: '/admin/database_name' },
-		{ name: 'Species', href: '/admin/species' },
-		{ name: 'Metadata', href: '/admin/meta' }
+		{ id: crypto.randomUUID(), name: 'Dashboard', href: '/admin/dashboard' },
+		{ id: crypto.randomUUID(), name: 'Create New user', href: '/admin/create_user' },
+		{ id: crypto.randomUUID(), name: 'Database name', href: '/admin/database_name' },
+		{ id: crypto.randomUUID(), name: 'Species', href: '/admin/species' },
+		{ id: crypto.randomUUID(), name: 'Metadata', href: '/admin/meta' }
 	];
 
 	let active_tab = admin_menu[0].name;
@@ -13,7 +13,7 @@
 <div class="settings__div">
 	<ul class="menu bg-base-200 w-56 rounded-box gap-2">
 		<li class="menu-title">Admin panel</li>
-		{#each admin_menu as { href, name }}
+		{#each admin_menu as { href, name, id } (id)}
 			<li>
 				<a
 					class:active={active_tab === name}
