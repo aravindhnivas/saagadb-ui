ae230f1f6def4425419d1a0aca39eb30d0c810df
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-01-08 10:54:04 -0500

    Update dropdown.svelte

--- src/lib/components/dropdown.svelte
+++ src/lib/components/dropdown.svelte
@@ -1,33 +1,44 @@
 <script lang="ts">
-	export let items = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla'];
-	export let selectedItem = items[0];
-	$: inputVal = '';
+	export let items = [
+		'Afghanistan',
+		'Albania',
+		'Algeria',
+		'Andorra',
+		'Angola',
+		'Anguilla',
+		'Afghanistan',
+		'Albania',
+		'Algeria',
+		'Andorra',
+		'Angola',
+		'Anguilla'
+	];
+	export let value = '';
+	export let label = 'Choose...';
+	let inputVal = '';
 	function onItemClicked(item) {
-		document.activeElement?.blur();
-		selectedItem = item;
+		// document.activeElement?.blur();
+		value = item;
 		inputVal = '';
 	}
 	$: filteredItems = items?.filter(function (item) {
 		return item.toLowerCase().includes(inputVal.toLowerCase());
 	});
+
+	// $: console.log(value);
 </script>
 
 <div class="dropdown">
-	<input class="input input-bordered" placeholder="Pick a country" bind:value={inputVal} />
-	<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+	<input class="input input-bordered" placeholder={label} bind:value />
 	<ul
-		tabindex="0"
 		class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 max-h-80 flex-nowrap overflow-auto"
 	>
+		<li>
+			<input class="w-full input input-bordered" placeholder="Search..." bind:value={inputVal} />
+		</li>
 		{#each filteredItems as item}
-			<!-- svelte-ignore a11y-click-events-have-key-events -->
-			<!-- svelte-ignore a11y-no-static-element-interactions -->
-			<!-- svelte-ignore a11y-no-static-element-interactions -->
-			<!-- svelte-ignore a11y-missing-attribute -->
-			<!-- svelte-ignore a11y-missing-attribute -->
 			<li>
-				<!-- svelte-ignore a11y-click-events-have-key-events -->
-				<a on:click|preventDefault={() => onItemClicked(item)}>{item}</a>
+				<button on:click={() => onItemClicked(item)}>{item}</button>
 			</li>
 		{/each}
 	</ul>
