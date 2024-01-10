287ce6da07b44edac36b5d7538a0f54b6d5a2c55
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-01-10 16:52:46 -0500

    Update +page.svelte

--- src/routes/admin/database_name/add_linelist/+page.svelte
+++ src/routes/admin/database_name/add_linelist/+page.svelte
@@ -3,23 +3,11 @@
 	import { linelistSchema } from '$lib/utils/schemas';
 	import FormComponent from '$lib/components/forms/form-component.svelte';
 	import * as Form from '$lib/components/ui/form';
-	import { type FormOptions } from 'formsnap';
-	import { toast } from 'svelte-sonner';
 
 	export let data: PageData;
-
-	const options: FormOptions<typeof linelistSchema> = {
-		// resetForm: true,
-		onResult: ({ result }) => {
-			const { form, ...rest } = result?.data;
-			if (form.valid) {
-				toast.success('Species added!');
-			}
-		}
-	};
 </script>
 
-<FormComponent class="p-2" schema={linelistSchema} form={data.form} let:config {options}>
+<FormComponent class="p-2" schema={linelistSchema} form={data.form} let:config>
 	<Form.Field {config} name="linelist_name">
 		<Form.Item class="max-w-md">
 			<Form.Label>Name</Form.Label>
