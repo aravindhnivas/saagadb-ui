b36b187d458143980a773aa99c671ecc12c9c979
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-01-20 20:52:43 -0500

    Update +server.ts

--- src/routes/uploads/[name]/[id]/+server.ts
+++ src/routes/uploads/[name]/[id]/+server.ts
@@ -1,8 +1,8 @@
 import type { RequestHandler } from './$types';
-import { env } from '$env/dynamic/private';
 import { error, text } from '@sveltejs/kit';
+import { DB_ORIGIN } from '$lib/server';
 
-const bib_url = `${env.DB_PROTOCOL}://${env.DB_HOST}:${env.DB_PORT}/static/media/uploads`;
+const bib_url = `${DB_ORIGIN}/static/media/uploads`;
 export const GET: RequestHandler = async ({ params }) => {
 	const fetch_url = new URL(`${bib_url}/${params.name}/${params.id}`);
 	const res = await fetch(fetch_url);
