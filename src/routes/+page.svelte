4a30063d0b9f760d2e5dca2dcdb2627d24aa7188
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-01-22 13:11:05 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { base } from '$app/paths';
+	import { Button } from '$lib/components/ui/button';
 </script>
 
 <div class="hero bg-base-200 card shadow-xl h-full">
@@ -7,7 +8,7 @@
 		<div class="max-w-md">
 			<h1 class="text-5xl font-bold">SAAGAdb</h1>
 			<p class="py-6">Species-Agnostic Automated Gas Analyzer</p>
-			<a href="{base}/species" class="btn btn-primary">Get Started</a>
+			<Button><a href="{base}/species">Get Started</a></Button>
 		</div>
 	</div>
 </div>
