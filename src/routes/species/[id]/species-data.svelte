db668b350fec45a0c3b266b153529dee88fb399e
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-01-25 14:33:52 -0500

    Update species-data.svelte

--- src/routes/species/[id]/species-data.svelte
+++ src/routes/species/[id]/species-data.svelte
@@ -20,9 +20,7 @@
 			<div>{@html svg}</div>
 			<div class="flex gap-4">
 				<span>{@html species.name_html} molecular structure </span>
-				<a class="btn btn-sm bg-slate-300" href={url} download="{species.iupac_name}.svg"
-					><Download /></a
-				>
+				<a href={url} download="{species.iupac_name}.svg"><Download /></a>
 			</div>
 		</div>
 	{:else}
