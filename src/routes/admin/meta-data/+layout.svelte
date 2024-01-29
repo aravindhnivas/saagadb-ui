cef3a5bc3d152a883ab20b75d44661b7ad000d8c
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-01-29 08:41:01 -0500

    Update +layout.svelte

--- src/routes/admin/meta-data/+layout.svelte
+++ src/routes/admin/meta-data/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import * as Tabs from '$lib/components/ui/tabs';
 	import { metadata_items } from '$lib/schemas/metadata';
+	import Dropfile from '$lib/components/file-drop.svelte';
 	let value = metadata_items[0].value;
 </script>
 
+<!-- <Dropfile update_form={false} /> -->
 <Tabs.Root {value}>
 	<Tabs.List class="grid w-full grid-cols-4">
 		{#each metadata_items as { value, name }}
