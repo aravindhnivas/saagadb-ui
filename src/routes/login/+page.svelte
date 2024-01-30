56571ec323554514bcca66a1957f121f2c2268a8
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-01-30 14:11:47 -0500

    Update +page.svelte

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+	import { EyeOff, Eye } from 'lucide-svelte';
 	import type { PageData } from './$types';
 	import * as Form from '$lib/components/ui/form';
 	import * as Card from '$lib/components/ui/card';
 	import loginSchema from '$lib/schemas/login';
 	export let data: PageData;
+
+	let show_password = false;
 </script>
 
 <div class="w-full flex items-center justify-center">
@@ -17,14 +20,23 @@
 				<Form.Field {config} name="email">
 					<Form.Item>
 						<Form.Label>Email</Form.Label>
-						<Form.Input type="email" />
+						<Form.Input type="email" class="w-[300px]" />
 						<Form.Validation />
 					</Form.Item>
 				</Form.Field>
 				<Form.Field {config} name="password">
 					<Form.Item>
 						<Form.Label>Password</Form.Label>
-						<Form.Input type="password" />
+						<div class="flex items-center gap-4">
+							<Form.Input class="w-[300px]" type={show_password ? 'string' : 'password'} />
+							<button type="button" on:click={() => (show_password = !show_password)}>
+								{#if show_password}
+									<Eye />
+								{:else}
+									<EyeOff />
+								{/if}
+							</button>
+						</div>
 						<Form.Validation />
 					</Form.Item>
 				</Form.Field>
