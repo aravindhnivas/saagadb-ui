824342dac0f5e9038e9437a06838e048e4bc8c97
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-02-05 13:27:05 -0500

    Update +page.svelte

--- src/routes/admin/dashboard/+page.svelte
+++ src/routes/admin/dashboard/+page.svelte
@@ -30,35 +30,37 @@
 
 		<UploadStatus {user} />
 
-		{#await data.fetch_approving_users()}
-			<Loader fetching={true} />
-		{:then approving_users}
-			<h1 class="text-2xl font-bold">
-				Dependent user - approve the data uploaded by the following users
-			</h1>
-			{#if approving_users.length > 0}
-				<Table.Root class="max-w-md">
-					<Table.Body>
-						{#each approving_users as approving_user, ind}
-							<Table.Row>
-								<Table.Cell class="p-0.5">{ind + 1}</Table.Cell>
-								<Table.Cell class="font-medium p-0.5">
-									<a
-										href="{base}/admin/dashboard/approve-data/{approving_user.id}"
-										class="hover:underline">{approving_user.name}</a
-									>
-								</Table.Cell>
-								<Table.Cell class="p-0.5">{approving_user.email}</Table.Cell>
-							</Table.Row>
-						{/each}
-					</Table.Body>
-				</Table.Root>
-			{:else}
-				<span>No approving users</span>
-			{/if}
-		{:catch error}
-			<AlertBox {error} />
-		{/await}
+		{#if user.is_staff}
+			{#await data.fetch_approving_users()}
+				<Loader fetching={true} />
+			{:then approving_users}
+				<h1 class="text-2xl font-bold">
+					Dependent user - approve the data uploaded by the following users
+				</h1>
+				{#if approving_users.length > 0}
+					<Table.Root class="max-w-md">
+						<Table.Body>
+							{#each approving_users as approving_user, ind}
+								<Table.Row>
+									<Table.Cell class="p-0.5">{ind + 1}</Table.Cell>
+									<Table.Cell class="font-medium p-0.5">
+										<a
+											href="{base}/admin/dashboard/approve-data/{approving_user.id}"
+											class="hover:underline">{approving_user.name}</a
+										>
+									</Table.Cell>
+									<Table.Cell class="p-0.5">{approving_user.email}</Table.Cell>
+								</Table.Row>
+							{/each}
+						</Table.Body>
+					</Table.Root>
+				{:else}
+					<span>No approving users</span>
+				{/if}
+			{:catch error}
+				<AlertBox {error} />
+			{/await}
+		{/if}
 	{:else}
 		<p>Invalid user</p>
 	{/if}
