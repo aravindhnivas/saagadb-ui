c2d221667c2f6c1edc57700926e0f37f23c7fb7d
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-02-07 12:58:04 -0500

    fetches only the approved metadata

--- src/routes/species/[id]/+layout.ts
+++ src/routes/species/[id]/+layout.ts
@@ -2,17 +2,19 @@ import { base } from '$app/paths';
 import type { LayoutLoad } from './$types';
 
 export const load: LayoutLoad = async ({ fetch, params }) => {
-	const fetch_data = async () => {
-		const [species, meta] = await Promise.all([
-			fetch(`${base}/api/data/species/${params.id}`).then((res) => res.json()),
-			fetch(`${base}/api/data/species-metadata?species=${params.id}`).then((res) => res.json())
-		]);
-
-		// console.log({ species, meta });
-		return { species, meta } as {
-			species: Species;
-			meta: SpeciesMetadata[];
-		};
+	const fetch_data = async <T>(url: string) => {
+		const res = await fetch(url);
+		return res.json() as T;
 	};
-	return { load_species_metadata: fetch_data() };
+
+	const fetch_species_and_metadata = async () => {
+		const [species, meta] = await Promise.all([
+			fetch_data<Species[]>(`${base}/api/data/species/${params.id}`),
+			fetch_data<SpeciesMetadata[]>(
+				`${base}/api/data/species-metadata?species=${params.id}&approved=true`
+			)
+		]);
+		return { species, meta };
+	};
+	return { load_species_metadata: fetch_species_and_metadata() };
 };
