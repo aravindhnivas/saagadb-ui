a7b74c45f52f4105a6f85a851cc97e5c96e6a9d1
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-02-16 17:15:59 -0500

    Update +page.svelte

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -4,13 +4,11 @@
 	import * as Card from '$lib/components/ui/card';
 	import loginSchema from '$lib/schemas/login';
 	import { Checkbox } from '$lib/components/ui/checkbox';
-	import { Loader2 } from 'lucide-svelte';
 	import { toast } from 'svelte-sonner';
 
 	export let data: PageData;
-
 	let show_password = false;
-	console.log(data);
+	// console.log(data);
 </script>
 
 <div class="w-full flex items-center justify-center">
