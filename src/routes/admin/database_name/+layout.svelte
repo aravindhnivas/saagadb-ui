10d82e3776923bb98765ba8ea2518c797a0722d9
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-02-22 11:14:03 -0500

    Update +layout.svelte

--- src/routes/admin/database_name/+layout.svelte
+++ src/routes/admin/database_name/+layout.svelte
@@ -2,12 +2,12 @@
 	import { base } from '$app/paths';
 	import { page } from '$app/stores';
 	import { v4 as uuidv4 } from 'uuid';
-	import type { LayoutData } from './$types';
+
 	const db_menu = [
 		{ id: uuidv4(), name: 'linelist', href: base + '/admin/database_name/linelist' },
 		{ id: uuidv4(), name: 'add_linelist', href: base + '/admin/database_name/add-linelist' }
 	];
-	export let data: LayoutData;
+
 	let active_tab = db_menu.find((f) => $page.url.pathname.startsWith(f.href))?.name;
 </script>
 
