85ffb0adaebe6ab168e8a787a465d67ddff49f50
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-02-25 18:09:14 -0500

    logout to GET request from POST

--- src/routes/logout/+server.ts
+++ src/routes/logout/+server.ts
@@ -3,7 +3,8 @@ import { logged_in } from '$lib/utils/stores';
 import type { RequestHandler } from './$types';
 import { delete_token } from '$lib/server/cookies';
 import { base } from '$app/paths';
-export const POST: RequestHandler = async ({ cookies }) => {
+
+export const GET: RequestHandler = async ({ cookies }) => {
 	logged_in.set('');
 	delete_token({ cookies });
 	redirect(303, base + '/');
