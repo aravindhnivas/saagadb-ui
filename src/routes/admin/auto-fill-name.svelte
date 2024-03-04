db23a8678ec7892c2adaee6543ff9748602a3544
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-03-04 11:05:02 -0500

    bug fix: using sessionStorage only in browser mode

--- src/routes/admin/auto-fill-name.svelte
+++ src/routes/admin/auto-fill-name.svelte
@@ -7,18 +7,33 @@
 	import { toast } from 'svelte-sonner';
 	import { base } from '$app/paths';
 	import Loader from '$lib/components/utils/loader.svelte';
-	// import { CDMS, JPL } from 'cdms-jpl-api';
+	import { browser } from '$app/environment';
 	import { CDMS, JPL } from '$lib/utils/cdms-jpl-parser';
-	import { sessionWritable } from '@macfja/svelte-persistent-store';
+	import { sessionWritable, type PersistentStore } from '@macfja/svelte-persistent-store';
+	import { onMount } from 'svelte';
 
 	export let callback: (db: string, data: Object) => void;
 
 	let className = '';
 	export { className as class };
 
+	// console.log('auto-fill-name', { browser });
+
+	let database_type: PersistentStore<'cdms' | 'jpl'>;
+	let molecule_tag: PersistentStore<string>;
+
+	let mounted = false;
+	onMount(() => {
+		if (browser) {
+			molecule_tag = sessionWritable<string>('molecule_tag', '');
+			database_type = sessionWritable<'cdms' | 'jpl'>('database_type', 'cdms');
+			// console.log('browser: auto-fill-name', { browser });
+		}
+		// console.log('mount: auto-fill-name', { browser });
+		mounted = true;
+	});
+
 	const { reset } = getForm();
-	const molecule_tag = sessionWritable<string>('molecule_tag', '');
-	const database_type = sessionWritable<'cdms' | 'jpl'>('database_type', 'cdms');
 	let fetching = false;
 	let status = '';
 	let link = '';
@@ -55,33 +70,35 @@
 	};
 </script>
 
-<div class="grid-fit-content items-end {className}">
-	<Combobox
-		label="database"
-		items={[
-			{ value: 'cdms', label: 'CDMS' },
-			{ value: 'jpl', label: 'JPL' }
-		]}
-		bind:value={$database_type}
-	/>
-	<div class="flex flex-col gap-1">
-		<Label>molecule tag</Label>
-		<Input
-			bind:value={$molecule_tag}
-			placeholder="Enter molecule tag"
-			on:keyup={async (e) => {
-				if (e.key === 'Enter') {
-					await fetch_from_database();
-				}
-			}}
+{#if mounted}
+	<div class="grid-fit-content items-end {className}">
+		<Combobox
+			label="database"
+			items={[
+				{ value: 'cdms', label: 'CDMS' },
+				{ value: 'jpl', label: 'JPL' }
+			]}
+			bind:value={$database_type}
 		/>
+		<div class="flex flex-col gap-1">
+			<Label>molecule tag</Label>
+			<Input
+				bind:value={$molecule_tag}
+				placeholder="Enter molecule tag"
+				on:keyup={async (e) => {
+					if (e.key === 'Enter') {
+						await fetch_from_database();
+					}
+				}}
+			/>
+		</div>
+		<Button on:click={async () => await fetch_from_database()}>Fetch & auto-fill</Button>
+		<Loader {fetching} />
+		{#if !fetching && status && link}
+			<p>{status}</p>
+			<a href={link} target="_blank" rel="noopener noreferrer" class="underline"
+				>{$database_type} source</a
+			>
+		{/if}
 	</div>
-	<Button on:click={async () => await fetch_from_database()}>Fetch & auto-fill</Button>
-	<Loader {fetching} />
-	{#if !fetching && status && link}
-		<p>{status}</p>
-		<a href={link} target="_blank" rel="noopener noreferrer" class="underline"
-			>{$database_type} source</a
-		>
-	{/if}
-</div>
+{/if}
