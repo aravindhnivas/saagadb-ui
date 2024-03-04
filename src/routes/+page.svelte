eba099dfc37b4c44af46634ccfa85ad511a42383
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-03-04 16:08:10 -0500

    new favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,22 +5,23 @@
 </script>
 
 <div class="hero card shadow-xl h-full">
-	<div class="hero-content text-center">
-		<div class="">
-			<h1 class="text-5xl font-bold">SAAGAdb</h1>
-			<p class="py-6">
-				The Species-agnostic Automated Gas Analyzer (SAAGA). A database of spectroscopic data serves
-				as the foundation of the automation pipeline for assigning spectral lines to species. While
-				there are existing databases available for use, we developed our custom database, named
-				SAAGAdb, and an application programming interface (API) to access the database to fulfil the
-				needs of SAAGA.
-			</p>
-			<Button variant="outline">
-				<a class="flex items-center gap-4" href="{base}/species">
-					<span>Get Started</span>
-					<ArrowRightCircle />
-				</a>
-			</Button>
-		</div>
+	<div class="hero-content text-center grid justify-items-center">
+		<!-- <div> -->
+		<img src="{base}/favicon.png" alt="favicon" class="w-25 h-25" />
+		<h1 class="text-5xl font-bold">SAAGAdb</h1>
+		<p class="py-6">
+			The Species-agnostic Automated Gas Analyzer (SAAGA). A database of spectroscopic data serves
+			as the foundation of the automation pipeline for assigning spectral lines to species. While
+			there are existing databases available for use, we developed our custom database, named
+			SAAGAdb, and an application programming interface (API) to access the database to fulfil the
+			needs of SAAGA.
+		</p>
+		<Button variant="outline">
+			<a class="flex items-center gap-4" href="{base}/species">
+				<span>Get Started</span>
+				<ArrowRightCircle />
+			</a>
+		</Button>
+		<!-- </div> -->
 	</div>
 </div>
