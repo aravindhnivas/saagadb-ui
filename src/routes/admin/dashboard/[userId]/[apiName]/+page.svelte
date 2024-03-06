d2aaa11ed0a7094b85263f4a73e5d3a17595cabb
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-03-06 13:39:54 -0500

    uploaded info

--- src/routes/admin/dashboard/[userId]/[apiName]/+page.svelte
+++ src/routes/admin/dashboard/[userId]/[apiName]/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-	import { ArrowBigLeft } from 'lucide-svelte/icons';
+	import { api_fields } from './api-fields';
+	import { AlertTriangle, ArrowBigLeft, CheckCheck } from 'lucide-svelte/icons';
 	import type { PageData } from './$types';
 	import { Button } from '$lib/components/ui/button';
+	import { page } from '$app/stores';
+	import * as Table from '$lib/components/ui/table';
 
 	export let data: PageData;
+	const fields = api_fields[$page.params.apiName];
 </script>
 
 <Button
@@ -16,6 +20,43 @@
 	<ArrowBigLeft /> Go back
 </Button>
 
-<pre>
-    {JSON.stringify(data.metadata, null, 2)}
-</pre>
+<h1 class="text-xl font-medium text-center">
+	Uploaded {$page.params.apiName}
+</h1>
+
+<div class="auto-fill space-y-2">
+	{#each data.metadata as metadata}
+		<div class="grid border-solid border-2 p-5">
+			<Table.Root class="text-md max-w-2xl">
+				<Table.Body>
+					{#each fields as { name, label }}
+						<Table.Row>
+							{#if name === 'approved'}
+								{#if metadata[name]}
+									<Table.Cell class="p-0.5 text-green-700"><CheckCheck /></Table.Cell>
+									<Table.Cell class="p-0.5 text-green-700">APPROVED</Table.Cell>
+								{:else}
+									<Table.Cell class="p-0.5 text-red"><AlertTriangle /></Table.Cell>
+									<Table.Cell class="p-0.5 text-red">PENDING APPROVAL</Table.Cell>
+								{/if}
+							{:else}
+								<Table.Cell class="p-0.5">{@html label}</Table.Cell>
+								<Table.Cell class="p-0.5">
+									{metadata[name] ?? '-'}
+								</Table.Cell>
+							{/if}
+						</Table.Row>
+					{/each}
+				</Table.Body>
+			</Table.Root>
+		</div>
+	{/each}
+</div>
+
+<style>
+	.auto-fill {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
+		grid-gap: 1rem;
+	}
+</style>
---
+++ src/routes/admin/dashboard/[userId]/[apiName]/api-fields.ts
@@ -0,0 +1,31 @@
+export const api_fields = {
+	species: [
+		{ name: 'name', label: 'Name' },
+		{ name: 'iupac_name', label: 'IUPAC Name' },
+		{ name: 'name_formula', label: 'Name Formula' },
+		{ name: 'name_html', label: 'Name HTML' }
+	],
+	'species-metadata': [
+		{ name: 'approved', label: 'Approved' },
+		{ name: 'category', label: 'Category' },
+		{ name: 'degree_of_freedom', label: 'Degree of Freedom' },
+		{ name: 'hyperfine', label: 'Hyperfine' },
+		{ name: 'mu_a', label: 'μ<sub>a</sub> / D' },
+		{ name: 'mu_b', label: 'μ<sub>b</sub> / D' },
+		{ name: 'mu_c', label: 'μ<sub>c</sub> / D' },
+		{ name: 'a_const', label: 'A / MHz' },
+		{ name: 'b_const', label: 'B / MHz' },
+		{ name: 'c_const', label: 'C / MHz' }
+	],
+	reference: [
+		{ name: 'doi', label: 'DOI' },
+		{ name: 'ref_url', label: 'Reference URL' }
+	],
+	'meta-reference': [
+		{ name: 'approved', label: 'Approved' },
+		{ name: 'doi', label: 'DOI' },
+		{ name: 'ref_url', label: 'Reference URL' },
+		{ name: 'dipole_moment', label: 'Dipole Moment' },
+		{ name: 'spectrum', label: 'Spectrum' }
+	]
+};
