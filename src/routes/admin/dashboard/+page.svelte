13ee2e22bbf1af48a4013e2caf65145baf9d0e39
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-03-06 12:19:55 -0500

    bug fix

--- src/routes/admin/dashboard/+page.svelte
+++ src/routes/admin/dashboard/+page.svelte
@@ -11,39 +11,35 @@
 	import { invalidate } from '$app/navigation';
 	import { Button } from '$lib/components/ui/button';
 	import { toast } from 'svelte-sonner';
+	import { writable, sessionWritable } from '@macfja/svelte-persistent-store';
 
 	export let data: PageData;
 	setContext('approve_btn', false);
-	// $: console.log(data.user);
 	$: if (data.user?.name) logged_in.set(data.user.name);
+	const active_tab = sessionWritable('dashboard-active-tab', 'upload-statistics');
 </script>
 
 {#if data.user}
-	<Tabs.Root value="upload-statistics">
+	<Tabs.Root value={$active_tab}>
 		<Tabs.List class="grid w-full grid-cols-2 max-w-md">
-			<Tabs.Trigger value="upload-statistics">Upload statistics</Tabs.Trigger>
-			<Tabs.Trigger value="approve-data">Approve data</Tabs.Trigger>
+			<Tabs.Trigger value="upload-statistics" on:click={() => ($active_tab = 'upload-statistics')}
+				>Upload statistics</Tabs.Trigger
+			>
+			<Tabs.Trigger value="approve-data" on:click={() => ($active_tab = 'approve-data')}
+				>Approve data</Tabs.Trigger
+			>
 		</Tabs.List>
 
 		<Tabs.Content value="upload-statistics">
 			<Card.Root>
 				<Card.Header>
-					<div class="flex gap-4 items-center">
-						<Card.Title>Upload statistics</Card.Title>
-						<Button
-							class="ml-auto"
-							on:click={async () => {
-								await invalidate('fetch:upload_count');
-								toast.success('Data re-fetched');
-							}}>Re-fetch data</Button
-						>
-					</div>
+					<Card.Title>Upload statistics</Card.Title>
 					<Card.Description>
 						You can see the statistics of the data uploaded by you and the approval status
 					</Card.Description>
 				</Card.Header>
-				<Card.Content class="space-y-2">
-					<div class="alert">
+				<Card.Content>
+					<div class="alert mb-2">
 						<!-- {#await data.fetch_approver then approver} -->
 						{#if data.user.approver_name}
 							{@const approver_name = data.user.approver_name}
@@ -66,28 +62,35 @@
 						show_header={false}
 					/>
 				</Card.Content>
+				<Card.Footer>
+					<Button
+						class="ml-auto"
+						on:click={async () => {
+							await invalidate('fetch:upload_count');
+							toast.success('Data re-fetched');
+						}}>Re-fetch data</Button
+					>
+				</Card.Footer>
 			</Card.Root>
 		</Tabs.Content>
 		<Tabs.Content value="approve-data">
 			{#if data.fetch_approving_users && (data.user.is_staff || data.user.is_superuser)}
 				<Card.Root>
 					<Card.Header>
-						<!-- <Card.Title>Dependent users</Card.Title> -->
-						<div class="flex">
-							<Card.Title>Dependent users</Card.Title>
-							<Button
-								class="ml-auto"
-								on:click={async () => {
-									await invalidate('fetch:approving_users');
-									toast.success('Data re-fetched');
-								}}>Re-fetch data</Button
-							>
-						</div>
+						<Card.Title>Dependent users</Card.Title>
 						<Card.Description>Approve the data uploaded by the following users</Card.Description>
 					</Card.Header>
-					<Card.Content class="space-y-2"></Card.Content>
-					<Card.Footer>
+					<Card.Content>
 						<DependentUser fetch_approving_users={data.fetch_approving_users(data.user)} />
+					</Card.Content>
+					<Card.Footer>
+						<Button
+							class="ml-auto"
+							on:click={async () => {
+								await invalidate('fetch:approving_users');
+								toast.success('Data re-fetched');
+							}}>Re-fetch data</Button
+						>
 					</Card.Footer>
 				</Card.Root>
 			{:else}
