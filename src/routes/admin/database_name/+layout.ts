4f334b67ba510fbd4aedfc5f2833487cc79a44af
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2024-12-09 13:33:19 -0500

    Update +layout.ts

--- src/routes/admin/database_name/+layout.ts
+++ src/routes/admin/database_name/+layout.ts
@@ -2,7 +2,7 @@ import type { LayoutLoad } from './$types';
 import { error } from '@sveltejs/kit';
 
 export const load: LayoutLoad = async ({ locals }) => {
-	if (!locals.user.is_staff) {
+	if (!locals?.user.is_staff) {
 		error(403, { message: 'Requires admin access', title: 'Forbidden' });
 	}
 };
