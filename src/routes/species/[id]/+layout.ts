d3287c23de983192b3b9fd4c32079ced85452962
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2025-03-28 15:35:54 -0400

    species_metadata showing pending and approved data

--- src/routes/species/[id]/+layout.ts
+++ src/routes/species/[id]/+layout.ts
@@ -8,13 +8,14 @@ export const load: LayoutLoad = async ({ fetch, params }) => {
 	};
 
 	const fetch_species_and_metadata = async () => {
-		const [species, meta] = await Promise.all([
+		const [species, species_metadata] = await Promise.all([
 			fetch_data<Species>(`${base}/api/data/species/${params.id}`),
-			fetch_data<SpeciesMetadata[]>(
-				`${base}/api/data/species-metadata?species=${params.id}&status=approved`
-			)
+			fetch_data<SpeciesMetadata[]>(`${base}/api/data/species-metadata?species=${params.id}`)
 		]);
-		return { species, meta };
+
+		const species_metadata_approved = species_metadata.filter((f) => f.status === 'approved');
+		const species_metadata_pending = species_metadata.filter((f) => f.status === 'pending');
+		return { species, species_metadata, species_metadata_approved, species_metadata_pending };
 	};
 	return { load_species_metadata: fetch_species_and_metadata() };
 };
--- src/routes/species/[id]/+page.svelte
+++ src/routes/species/[id]/+page.svelte
@@ -10,15 +10,21 @@
 
 {#await data.load_species_metadata}
 	<Loader />
-{:then { species, meta }}
+{:then { species, species_metadata, species_metadata_approved, species_metadata_pending }}
 	{#if species?.message}
 		<AlertBox message={species.message} variant="destructive" />
 	{:else if species}
 		<SpeciesData {species} user={data.user} />
-		{#if meta.length > 0}
-			<MetaTable species_metadata={meta} />
-		{:else}
+		{#if species_metadata.length === 0}
 			<AlertBox message="No data available for this species" />
+		{:else if species_metadata_pending.length > 0}
+			<AlertBox
+				title="Awaiting review from approver"
+				message="{species_metadata_pending.length} review pending"
+			/>
+		{/if}
+		{#if species_metadata_approved.length > 0}
+			<MetaTable species_metadata={species_metadata_approved} />
 		{/if}
 	{/if}
 {:catch error}
