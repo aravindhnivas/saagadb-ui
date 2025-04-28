1cdcd049efcbc482dbf4bf8f9e3f9ee6fde7b2a0
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2025-04-27 23:51:18 -0400

    license year update

--- src/lib/layout/footer.svelte
+++ src/lib/layout/footer.svelte
@@ -1,5 +1,5 @@
 <footer class="footer footer-center p-4 bg-base-300 text-base-content">
 	<aside>
-		<p>Copyright © 2024 - All right reserved by Mcguire Group</p>
+		<p>Copyright © 2025 - All right reserved by Mcguire Group</p>
 	</aside>
 </footer>
